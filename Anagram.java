9839fbf9fcb332907bdddeaa23d21fe9a69c52be
vvishnoi19 <148255148+vvishnoi19@users.noreply.github.com>
2023-10-19 20:30:45 +0530

    Update and rename README.md to Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,19 @@
+package stack;
+import java.util.*;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER 1ST STRING");
+        String r= sc.nextLine();
+        System.out.println("ENTER 2nd STRING");
+        String s= sc.nextLine();
+        
+        if(r.length()==s.length()){
+            
+
+        }
+   
+    }
+    
+}
