293c7e050c363484dcd8d7d8274ebd6718a801da
vvishnoi19 <148255148+vvishnoi19@users.noreply.github.com>
2023-10-19 20:54:32 +0530

    Create problem.java

---
+++ problem.java
@@ -0,0 +1,26 @@
+package stack;
+
+import java.util.Scanner;
+
+public class problem {
+    
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        float p=sc.nextInt();
+        float s=sc.nextInt();
+        System.out.println("press (1)for addition,press(2)for substarction,press (3)for multiplication,press(4)for :");
+        int n=sc.nextInt();
+        sc.close(); //to debug the resource leak error on sc
+        switch(n){
+            case 1:
+            {System.out.print("Addition:")
+            ;}
+        }
+
+
+        
+    }
+    
+}
