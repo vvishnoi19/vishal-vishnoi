ea8bfa574e8d1d00d12db42e650c5f81cd9b1664
vvishnoi19 <148255148+vvishnoi19@users.noreply.github.com>
2023-10-19 20:33:04 +0530

    Create Greatestno.java

---
+++ Greatestno.java
@@ -0,0 +1,13 @@
+package stack;
+
+public class Greatestnum { 
+    public static void main(String[] args) {
+        
+    
+    int a=13; 
+    int b=12;
+    int c=10;
+System.out.println(( (a>b)&&(a>c)?"A  is greatest":(b>c)?"B is greater":"C is greater"));
+    
+}
+}
