d46a7cf00a106c401218beb8bb5ab3e007189af6
vvishnoi19 <148255148+vvishnoi19@users.noreply.github.com>
2023-10-19 20:36:12 +0530

    Create Rectarea.java

---
+++ Rectarea.java
@@ -0,0 +1,13 @@
+package stack;
+import java.util.*;
+public class Rectarea {public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter lenght and breadth");
+    float l= sc.nextFloat();
+    float b= sc.nextFloat();
+    
+    System.out.print("Area of rectangle=");
+    System.out.println(l*b);
+}
+    
+}
