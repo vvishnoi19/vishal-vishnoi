7d8394bb1c303fd3dffd207b3eed833b098c94d8
vvishnoi19 <148255148+vvishnoi19@users.noreply.github.com>
2023-10-19 20:55:44 +0530

    Create swapusing3variable.java

---
+++ swapusing3variable.java
@@ -0,0 +1,19 @@
+package stack;
+
+public class swapusing3variable {public static void main(String[] args) {
+    int a=12;
+    int b=10;
+    int c;
+    System.out.println("variables before swapping");
+    System.out.println(a);
+    System.out.println(b);
+    c=a;
+    a=b;
+    b=c;
+    System.out.println("variables after swapping");
+    System.out.println(a);
+    System.out.println(b);
+    
+}
+    
+}
