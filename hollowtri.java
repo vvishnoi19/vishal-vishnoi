fd5339a73fb6c47a3ca63ae0f41893bd5399462c
vvishnoi19 <148255148+vvishnoi19@users.noreply.github.com>
2023-10-19 20:47:21 +0530

    Create hollowtri.java

---
+++ hollowtri.java
@@ -0,0 +1,18 @@
+package apnikaksha;
+import java.util.*;
+public class hollowtri {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER ROWS:");
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {for (int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        
+        System.out.println();
+        }
+        
+    }
+    
+}
